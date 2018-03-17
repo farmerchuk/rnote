cba7f2afd6b5e1f412c3dc86077c3288966f40cc
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2018-03-17 09:38:24 -0700

    added note validations and jquery for scrolling to active note

---
+++ public/javascripts/application.js
@@ -0,0 +1,11 @@
+$.fn.scrollView = function () {
+  return this.each(function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 0);
+  });
+}
+
+$(document).ready(function() {
+  $('#active-note').scrollView();
+});
